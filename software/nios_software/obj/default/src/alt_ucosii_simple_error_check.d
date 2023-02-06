3750c827e9b8fb634439294d2607ec2382de145d
David García <davidgarcia.elcau@gmail.com>
2023-02-06 18:21:35 +0100

    Test del gitignore

---
+++ software/nios_software/obj/default/src/alt_ucosii_simple_error_check.d
@@ -0,0 +1,50 @@
+obj/default/src/alt_ucosii_simple_error_check.o: \
+ src/alt_ucosii_simple_error_check.c \
+ c:\users\dagarbl2\documents\github\proyecto_chs\software\nios_software_bsp\hal\inc\includes.h \
+ c:\users\dagarbl2\documents\github\proyecto_chs\software\nios_software_bsp\hal\inc\os_cpu.h \
+ c:\users\dagarbl2\documents\github\proyecto_chs\software\nios_software_bsp\hal\inc\sys/alt_irq.h \
+ c:\users\dagarbl2\documents\github\proyecto_chs\software\nios_software_bsp\hal\inc\nios2.h \
+ c:\users\dagarbl2\documents\github\proyecto_chs\software\nios_software_bsp\hal\inc\alt_types.h \
+ C:/Users/dagarbl2/Documents/GitHub/proyecto_CHS/software/nios_software_bsp/system.h \
+ C:/Users/dagarbl2/Documents/GitHub/proyecto_CHS/software/nios_software_bsp/linker.h \
+ c:\users\dagarbl2\documents\github\proyecto_chs\software\nios_software_bsp\hal\inc\priv\alt_legacy_irq.h \
+ c:\users\dagarbl2\documents\github\proyecto_chs\software\nios_software_bsp\hal\inc\sys\alt_irq.h \
+ c:\users\dagarbl2\documents\github\proyecto_chs\software\nios_software_bsp\ucosii\inc\os_cfg.h \
+ c:\users\dagarbl2\documents\github\proyecto_chs\software\nios_software_bsp\hal\inc\sys\alt_alarm.h \
+ c:\users\dagarbl2\documents\github\proyecto_chs\software\nios_software_bsp\hal\inc\sys\alt_llist.h \
+ c:\users\dagarbl2\documents\github\proyecto_chs\software\nios_software_bsp\hal\inc\priv\alt_alarm.h \
+ c:\users\dagarbl2\documents\github\proyecto_chs\software\nios_software_bsp\ucosii\inc\ucos_ii.h \
+ c:\users\dagarbl2\documents\github\proyecto_chs\software\nios_software_bsp\hal\inc\os_cpu.h \
+ src/../inc/alt_ucosii_simple_error_check.h
+
+c:\users\dagarbl2\documents\github\proyecto_chs\software\nios_software_bsp\hal\inc\includes.h:
+
+c:\users\dagarbl2\documents\github\proyecto_chs\software\nios_software_bsp\hal\inc\os_cpu.h:
+
+c:\users\dagarbl2\documents\github\proyecto_chs\software\nios_software_bsp\hal\inc\sys/alt_irq.h:
+
+c:\users\dagarbl2\documents\github\proyecto_chs\software\nios_software_bsp\hal\inc\nios2.h:
+
+c:\users\dagarbl2\documents\github\proyecto_chs\software\nios_software_bsp\hal\inc\alt_types.h:
+
+C:/Users/dagarbl2/Documents/GitHub/proyecto_CHS/software/nios_software_bsp/system.h:
+
+C:/Users/dagarbl2/Documents/GitHub/proyecto_CHS/software/nios_software_bsp/linker.h:
+
+c:\users\dagarbl2\documents\github\proyecto_chs\software\nios_software_bsp\hal\inc\priv\alt_legacy_irq.h:
+
+c:\users\dagarbl2\documents\github\proyecto_chs\software\nios_software_bsp\hal\inc\sys\alt_irq.h:
+
+c:\users\dagarbl2\documents\github\proyecto_chs\software\nios_software_bsp\ucosii\inc\os_cfg.h:
+
+c:\users\dagarbl2\documents\github\proyecto_chs\software\nios_software_bsp\hal\inc\sys\alt_alarm.h:
+
+c:\users\dagarbl2\documents\github\proyecto_chs\software\nios_software_bsp\hal\inc\sys\alt_llist.h:
+
+c:\users\dagarbl2\documents\github\proyecto_chs\software\nios_software_bsp\hal\inc\priv\alt_alarm.h:
+
+c:\users\dagarbl2\documents\github\proyecto_chs\software\nios_software_bsp\ucosii\inc\ucos_ii.h:
+
+c:\users\dagarbl2\documents\github\proyecto_chs\software\nios_software_bsp\hal\inc\os_cpu.h:
+
+src/../inc/alt_ucosii_simple_error_check.h:
