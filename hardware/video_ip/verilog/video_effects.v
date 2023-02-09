26a459987ab337aa33d4c2386949d91b53ad905e
Juan <71964582+dpmj@users.noreply.github.com>
2023-02-09 20:59:43 +0100

    segunda entrada de vídeo de video_ip

--- hardware/video_ip/verilog/video_effects.v
+++ hardware/video_ip/verilog/video_effects.v
@@ -7,7 +7,7 @@ module video_effects (
     input wire clk,  // reloj
     input wire reset,  // reset
     
-    input wire [4:0] effect,  // indica el efecto de vídeo a aplicar. 5 bits
+    input wire [7:0] effect,  // indica el efecto de vídeo a aplicar. 5 bits
     
     // effect[0] : Sustitución de uno o varios colores por un valor constante (Chroma Key)
     //      Usar la variable effect_color_key para especificarlo
@@ -42,7 +42,8 @@ module video_effects (
     input wire [15:0] effect_color_key_threshold,  // indica la tolerancia de colores (+-rango por componente)
     input wire [15:0] effect_color_substitute,  // indica el valor RGB por el que sustituir el color eliminado
     
-    input wire [15:0] video_data_in,  // entrada de datos de vídeo, del avalon sink
+    input wire [15:0] video_data_in_foreground,  // entrada de datos de vídeo (camara de vídeo)
+    input wire [15:0] video_data_in_background,  // entrada de datos de vídeo (imagen de la SD)
     output reg [15:0] video_data_out  // salida de datos de vídeo, procesados, al avalon source
     
 );
@@ -57,15 +58,25 @@ reg [4:0] video_aux_gs;  // Valores intermedios de cálculos para filtro greysca
 always @(posedge clk)
 begin
 
-    video_data_proc = video_data_in;
     
     // Pueden aplicarse varios filtros simultáneamente.
     // Están definidos por prioridad secuencial: los primeros IFs se aplican antes.
     // Por eso se usa esta estructura de ifs encadenados y asignaciones combinacionales.
     
     
-    // 1. Sustitución de uno o varios colores por un valor constante (Chroma Key)
+    // 0. Seleccionar entre vídeo de la cámara (fore) o de la SD (back)
     if (effect[0] == 1'b1)
+    begin
+        video_data_proc = video_data_in_background;
+    end
+    else
+    begin
+        video_data_proc = video_data_in_foreground;
+    end
+    
+    
+    // 1. Sustitución de uno o varios colores por un valor constante (Chroma Key)
+    if (effect[1] == 1'b1)
     begin
         // Si (pixel & mask) menos (key & mask) es cero => está en rango => sustituirlo
         // (video_data_proc[15:11] >> 2) + (video_data_proc[10:6] >> 1) + (video_data_proc[4:0] >> 2);
@@ -77,7 +88,7 @@ begin
     
     
     // 2. Eliminación de uno o varios colores RGB
-    if (effect[1] == 1'b1)
+    if (effect[2] == 1'b1)
     begin
         case (effect_delete_rgb) 
             2'b01: video_data_proc = {5'b0, video_data_proc[10:5], video_data_proc[4:0]};
@@ -88,7 +99,7 @@ begin
     
     
     // 3. Escala de grises: Conversión de RGB a escala de grises
-    if (effect[2] == 1'b1)
+    if (effect[3] == 1'b1)
     begin
         // Grayscale por luminosidad aproximada: 0.25 * R; 0,5 * G; 0,25* B
         // Para el Green usamos 5 bits, los más significativos (el último es ignorado).
@@ -99,10 +110,8 @@ begin
     end
     
     
-    
-    
     // 4. Cuantificación: reducción de la precisión del color
-    if (effect[3] == 1'b1)
+    if (effect[4] == 1'b1)
     begin
         case (effect_quantif_level) 
             2'b01: video_data_proc = {video_data_proc[15:12],    video_data_proc[12],   video_data_proc[10:7], {2{video_data_proc[7]}}, video_data_proc[4:1],    video_data_proc[1]};
@@ -113,14 +122,14 @@ begin
     
     
     // 5. Negativo: invertir bits
-    if (effect[4] == 1'b1)
+    if (effect[5] == 1'b1)
     begin
         video_data_proc = ~video_data_proc;
     end
     
-    video_data_proc = video_data_proc;
     
     // Si no entra en ningún caso, simplemente se asigna a la salida.
+    video_data_proc = video_data_proc;
     video_data_out <= video_data_proc;
     
 end
